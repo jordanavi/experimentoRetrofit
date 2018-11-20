7dc469d97ef15e3047aa90c79d53bd350af76161
jordanavi <jordanavilela01@gmail.com>
2018-11-20 16:33:32 -0200

    Modelo

--- app/src/main/java/com/example/jorda/experimentoretrofit/MainActivity.java
+++ app/src/main/java/com/example/jorda/experimentoretrofit/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.jorda.experimentoretrofit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mUserIdTextView;
+    private TextView mIdTextView;
+    private TextView mTitleTextView;
+    private TextView mBodyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
